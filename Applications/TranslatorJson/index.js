80fa7cc53317c00e8f07e0af30b7fd60becb2508
Michael <Michael_veilis@hotmail.com>
2018-11-11 15:56:30 +0100

    configuration

--- Applications/TranslatorJson/index.js
+++
@@ -1,3 +0,0 @@
-import 'babel-core/register';
-import { main } from './src/main';
-main();
--- Applications/TranslatorJson/test/getMessageTest.js
+++
@@ -1,3 +0,0 @@
-//import { MessageTest } from '../test/MessageTest';
-const MessageTest = require('../test/MessageTest');
-console.log(MessageTest);
