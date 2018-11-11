07c5891ec0f57dcc348a49e5eed4fa5040b3e75d
Michael <Michael_veilis@hotmail.com>
2018-11-11 15:09:05 +0100

    RuleBase and JSON bank

---
+++ Applications/TranslatorJson/index.js
@@ -0,0 +1,3 @@
+import 'babel-core/register';
+import { main } from './src/main';
+main();
---
+++ Applications/TranslatorJson/test/getMessageTest.js
@@ -0,0 +1,3 @@
+//import { MessageTest } from '../test/MessageTest';
+const MessageTest = require('../test/MessageTest');
+console.log(MessageTest);
