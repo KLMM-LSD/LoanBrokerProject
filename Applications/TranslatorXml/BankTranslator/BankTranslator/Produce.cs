6b5ae7d1b9902cc12d83d15034ab1ae8aece97f1
MartinH5 <mart_h@live.dk>
2018-11-13 16:20:09 +0100

    Init XML translator

---
+++ Applications/TranslatorXml/BankTranslator/BankTranslator/Produce.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace BankTranslator
+{
+    class Produce
+    {
+        public static void Cain()
+        {
+            new Thread(() =>
+            {
+                 
+            }).Start();
+        }
+    }
+}
