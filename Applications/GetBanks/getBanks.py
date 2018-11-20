22229fa15086915743d1e93eecffd881690ae83c
Michael <Michael_veilis@hotmail.com>
2018-11-20 19:43:47 +0100

    made getBanks.py

---
+++ Applications/GetBanks/getBanks.py
@@ -0,0 +1,53 @@
+import zeep
+import pika
+import json
+
+def requestSoapService(body):
+    wsdl = 'vores rules.wsdl fil'
+    client = zeep.Client(wsdl=wsdl)
+    return client.service.GetDetails(int(body['loanAmount']), int(body['loanDuration']), int(body['creditScore']))
+
+def addBanks(banks, json):
+    json['banks']=[]
+    for bank in banks.split(','):
+        json['banks'].append(bank)
+    return json
+
+def addBankToQueue(banks):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('datdb.cphbusiness.dk'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='GroupB.getBanks', exchange_type='fanout', durable=True)
+
+    channel.basic_publish(exchange='GroupB.getBanks', routing_key='', body=json.dumps(banks))
+
+    print(" [X] Sent: " (banks))
+    connection.close()
+
+def connect():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('datdb.cphbusiness.dk'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='GroupB.creditscore.exchange', exchange_type='fanout')
+
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='GroupB.creditscore.exchange', queue=queue_name, routing_key='')
+
+    print(' [*] Waiting for answer. To exit press CTRL+C')
+
+    channel.basic_consume(callback, queue=queue_name)
+
+    channel.start_consuming()
+
+
+def callback(ch, method, properties, body):
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    body = body.decode('utf-8')
+    banks = requestSoapService(json.loads(body))
+    jsonBody = addBanks(banks, json.loads(body))
+    addBankToQueue(jsonBody)
+
+if __name__ == "__main__":
+    connect()
